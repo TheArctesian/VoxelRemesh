9490acc64a3df6c4e30bff685e41559a90e608fb
TheArctesian <stephen.d.okita@gmail.com>
2022-02-24 22:37:01 +0800

    redo

---
+++ 3d.py
@@ -0,0 +1,17 @@
+import bpy #blender 
+
+
+
+
+
+
+
+# This spawns a 3d object in the scene.
+# # pip install vpython 
+
+# import vpython as vp
+
+# ball = vp.sphere(pos=vp.vector(0,0,0), radius=1, color=vp.color.red)
+# while True:
+#     pass
+
